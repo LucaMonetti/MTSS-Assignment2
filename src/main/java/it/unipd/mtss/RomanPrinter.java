9dc351908b3f0f6f780597428e0597b3295361a3
LucaMonetti <monettiluca03@gmail.com>
2024-05-20 11:15:04 +0200

    Aggiunta stampa numeri primi 3 numeri interi closes #18 closes #4

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////
+// Luca Monetti 2069429
+// Victoria Nesterenco 2080818
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class RomanPrinter {
+
+    private static Map<Character, String[]> table = createMap();
+
+    private static Map<Character, String[]> createMap() {
+        Map<Character, String[]> out = new TreeMap<Character, String[]>();
+        out.put('I', new String[]{
+                "  _____  ",
+                " |_   _| ",
+                "   | |   ",
+                "   | |   ",
+                "  _| |_  ",
+                " |_____| "
+        });
+
+        return out;
+    }
+
+    public static String print(int num){
+        try {
+            return printAsciiArt(IntegerToRoman.convert(num));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "";
+        }
+    }
+    private static String printAsciiArt(String romanNumber){
+
+        String[] tempArr = {"", "", "", "", "", ""};
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < romanNumber.length(); i++) {
+            String[] value = table.get(romanNumber.charAt(i)).clone();
+
+            for (int j = 0; j < value.length; j++) {
+                tempArr[j] = tempArr[j] + value[j];
+            }
+        }
+
+        for (String s : tempArr) {
+            res.append(s).append("\n");
+        }
+
+        return res.toString();
+    }
+}
