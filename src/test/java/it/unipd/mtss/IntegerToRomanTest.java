468d490fe9e066a4086b4694d8ede2d4c6869041
Vic37492 <victoria.nesterenco@studenti.unipd.it>
2024-05-20 09:56:38 +0200

    Aggiunti test IntegerToRoman closes #1 closes #2

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,52 +1,11 @@
 package it.unipd.mtss;
 
-import static org.junit.Assert.*;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-
-
-@RunWith(Parameterized.class)
 
 public class IntegerToRomanTest {
-    
-    private int input;
-    private String expectedOutput;
-    
-    public IntegerToRomanTest(int input, String expectedOutput) {
-       this.input = input;
-       this.expectedOutput = expectedOutput;
+    @Test (expected = Exception.class)
+    public void test0() throws Exception {
+        String res = IntegerToRoman.convert(0);
     }
-
-    @Parameters
-    public static Collection testConditions() {
-       return Arrays.asList(new Object[][] { 
-             { 1, "I" }, 
-             { 2, "II" } });
-
 }
-    
-    @Test
-    public void test0() {
-        
-        try {
-            String res = IntegerToRoman.convert(input);
-            assertEquals(expectedOutput, res);
-        } catch (Exception e) {
-            System.out.println("Errore");
-        }
-    }
-
-
-}
-
-
-
-
